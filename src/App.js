f76ec1767c83947bc65efbb9bcc5a8fc985578d3
YOYO-Kira <1164020843@qq.com>
2019-08-07 10:54:50 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import Main from "./commponents/Main"
+import { NavLink } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
+import './app.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router className='App'>
+      <ul>
+        <li>
+          <NavLink
+            className='nav-item'
+            exact
+            activeStyle={{ color: "red" }}
+            to='/'
+          >
+            home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={{ color: "red" }} to='/about'>
+            about
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={{ color: "red" }} to='/users'>
+            users
+          </NavLink>
+        </li>
+      </ul>
+      <Main />
+    </Router>
+  )
 }
-
-export default App;
+export default App
